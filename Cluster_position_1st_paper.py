fd0ff42e7b3e78a7ecbe087078525157c1eeb2c1
Christina Schoettler <cschoettler1@sheffield.ac.uk>
2018-07-05 15:12:44 +0100

    Added ejected stars to plot code

--- Cluster_position_1st_paper.py
+++ Cluster_position_1st_paper.py
@@ -20,8 +20,6 @@ from function_file import read_in_any_datafiles, energy_stars, plots_legend, vel
 #reads in the .dat files from an inputfile list
 dflist, dname, nlines = read_in_any_datafiles('file.txt')
 
-#%%
-
 #calculates energy of each star in each snapshot and adds this in a column
 dflist = energy_stars(dflist, dname, nlines)
 
@@ -41,21 +39,24 @@ legend = plots_legend(dname)
 
 plt.style.use('seaborn-poster')
 
-j = 50
+j = 100
 
 for n in range(nlines):
     fig, ax = plt.subplots(figsize=(10., 10.))
 #    ax.set(xlim=(-20., 20.), ylim=(-20., 20.))
 
     ax.set_xlabel('x (pc)')
-    ax.set_ylabel('y (pc)')
+    ax.set_ylabel('z (pc)')
 
-    ax.scatter(dflist[n].loc[j,'radx'],dflist[n].loc[j,'rady'], s=5.0)
+    ax.scatter(dflist[n].loc[j,'radx'],dflist[n].loc[j,'radz'], s=5.0, c='grey')
+#    ax.scatter(dflist[n].loc[j,'rady'][(dflist[n].loc[j,'Unbound']== True)],dflist[n].loc[j,'radz']\
+#               [(dflist[n].loc[j,'Unbound']== True)], s=6.0, c='goldenrod', label='Ejected stars')
+  
 
 #    ax.scatter(dflist[n].loc[j,'radx'],dflist[n].loc[j,'rady'], s=10.0, label='Cluster stars')
 #    ax.scatter(dflist[n].loc[j,'radx'][(dflist[n].loc[j,'Unbound']== True) & (dflist[n].loc[j,'PM-velxy'] > 2.5)],dflist[n].loc[j,'rady']\
-#               [(dflist[n].loc[j,'Unbound']== True) & (dflist[n].loc[j,'PM-velxy'] > 2.5)], s=15.0, c='goldenrod', label='Ejected stars')
-
+#               [(dflist[n].loc[j,'Unbound']== True) & (dflist[n].loc[j,'PM-velxy'] > 2.5)], s=10.0, c='goldenrod', label='Ejected stars')
+#
     ax.tick_params(which='both', direction='in', top=True, right=True)
     plt.show()
 
@@ -65,8 +66,32 @@ for n in range(nlines):
 #
 #    for text in leg.get_texts():
 #        text.set_color('#002342')
-    plt.savefig('Cluster_check_%s.png' %dname[n])
-plt.show()
+    plt.savefig('Cluster_check_xz_%s.png' %dname[n])
+#plt.show()
+
+#%%
+
+import mpl_toolkits.mplot3d.axes3d as p3
+
+for n in range(nlines):
+    fig, ax = plt.subplots(figsize=(10., 10.))
+    ax = p3.Axes3D(fig)
+    
+    ax.set_xlabel('x [pc]')
+    ax.set_ylabel('y [pc]')
+    ax.set_zlabel('z [pc]')
+    
+    ax.scatter(dflist[n].loc[j, 'radx'],  dflist[n].loc[j, 'rady'], dflist[n].loc[j, 'radz'] , s=5.0, c='grey')
+    ax.scatter(dflist[n].loc[j,'radx'][(dflist[n].loc[j,'Unbound']== True)],dflist[n].loc[j,'rady']\
+           [(dflist[n].loc[j,'Unbound']== True)], dflist[n].loc[j,'radz'][(dflist[n].loc[j,'Unbound']== True)],\
+           s=6.0, c='goldenrod', label='Ejected stars')
+
+    ax.view_init(30, 135)
+    ax.grid(False)
+#ax.set(xlim=(-5., 5.), ylim=(-5., 5.), zlim=(-5., 5.))
+#ax.set(xlim=(-2., 2.), ylim=(-2., 2.), zlim=(-2., 2.))
+
+
 
 #%%
 
